cc72f705fd0e6391ca10c2b1ace0be113498e74b
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-06 15:09:27 +0300

    Add router transition

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view></router-view>
+  <transition name="fade" mode="out-in">
+    <router-view></router-view>
+  </transition>
 </template>
 
 <style lang="sass">
-
+.fade-enter-from, .fade-leave-to
+  opacity: 0
+.fade-enter-active, .fade-leave-active
+  transition: opacity .7s ease-in
 </style>
 
 <script>

0cb152a5fd1e47d493006c32cb8b76577280e69d
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-06 15:13:58 +0300

    Add signin and signup routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '../views/MainPage/MainPage.vue'
+import Signup from '../views/MainPage/Signup.vue'
+import Signin from '../views/MainPage/Signin.vue'
 
 const routes = [
   {
     path: '/',
     name: 'MainPage',
     component: MainPage
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: Signup
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: Signin
   }
 ]
 

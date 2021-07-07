0e044df49c5929e4964982a74a24d6944035e340
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-07 03:05:34 +0300

    Enable vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-//import store from './store'
+import store from './store'
 
 import './assets/styles/styles.sass'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')

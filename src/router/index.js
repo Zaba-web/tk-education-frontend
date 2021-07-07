6477b6645989816622b755779b0d7a9ca8228e69
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-07 03:06:02 +0300

    Add new routes and hook for permission check

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainPage from '../views/MainPage/MainPage.vue'
-import Signup from '../views/MainPage/Signup.vue'
-import Signin from '../views/MainPage/Signin.vue'
+
+import mainPageRoutes from '../static/routes/mainPageRoutes'
+import adminRoutes from '../static/routes/adminRoutes'
+
+import AdminLayout from '../views/Admin/AdminLayout.vue'
+
+import API from '../libs/api'
+
+const api = new API()
 
 const routes = [
-  {
-    path: '/',
-    name: 'MainPage',
-    component: MainPage
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: Signup
-  },
-  {
-    path: '/signin',
-    name: 'SignIn',
-    component: Signin
-  }
+	...mainPageRoutes,
+	{
+		path: '/admin',
+		name: 'adminLayout',
+		component: AdminLayout,
+		children: [
+			...adminRoutes
+		]
+	},
+	{
+		path: '/logout',
+		beforeEnter(){
+			api.userLogout().then(()=>{
+				router.push('/')
+			})
+		}
+	}
 ]
-
+  
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+
+	if(to.fullPath.includes("admin")) { 
+
+		if(!api.ifUserLogin()) {
+			next(from.path || '/')
+			return ;
+		}
+
+		api.getUserAccessLevel().then(userAccessLevel=>{
+			if(userAccessLevel < 2) {
+				next(from.path || '/')
+				return ;
+			}
+
+			next()
+		})
+		
+	} else {
+		next()
+	} 
 })
 
 export default router

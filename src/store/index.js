9606b4bc5dac7ab0b3a5579abf637371d4a29f58
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-07 03:06:24 +0300

    Add methods for user data

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
 import { createStore } from 'vuex'
+import API from '../libs/api'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+
+	},
+	getters: {
+		getUserData(state){
+			return state.user
+		}
+	},
+	mutations: {
+		SET_USER_DATA(state, payload) {
+			state.user = payload
+		}
+	},
+	actions: {
+		async loadUserData({commit}) {
+			const api = new API()
+
+			try {
+				const user = await api.getAllUserData()
+				commit('SET_USER_DATA', user)
+			} catch(error) {
+				console.error(error)
+			}
+		}
+	},
+	modules: {
+	}
 })

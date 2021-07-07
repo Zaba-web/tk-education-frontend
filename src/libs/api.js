76771b8e36edd91a54aa8409184613f3056bce98
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-07 03:04:55 +0300

    Add methods for fetching user data

--- src/libs/api.js
+++ src/libs/api.js
@@ -2,9 +2,20 @@ import axios from 'axios'
 
 const serverLocation = 'http://127.0.0.1:8000/api/'
 
-function API(){
-    this.post = (path, data, callback)=>{
-        let result = axios.post(`${serverLocation}${path}`, data)
+class API {
+    constructor(){
+        this.axiosInstance = axios.create()
+    }
+
+    authorize(){
+        if(localStorage.getItem('token')){
+            this.axiosInstance.defaults.headers.post['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+            this.axiosInstance.defaults.headers.get['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+        }
+    }
+
+    post(path, data, callback){
+        let result = this.axiosInstance.post(`${serverLocation}${path}`, data)
         
         if(!callback) 
             return result
@@ -12,15 +23,55 @@ function API(){
         result.then(response=>{
             callback(response)
         })
-    },
-    this.get = (path, params = '', callback)=>{
-        let result = axios.get(`${serverLocation}${path}/${params}`)
+    }
+
+    get(path, params = '', callback){
+        let result = this.axiosInstance.get(`${serverLocation}${path}/${params}`)
         
         if(!callback) 
             return result
     
         result.then(response=>{
             callback(response)
+        })
+    }
+
+    getUserAccessLevel(){
+        this.authorize()
+        return this.get('user/access_level').then(response => {
+            return response.data
+        })
+    }
+
+    ifUserLogin(){
+        if(localStorage.getItem('token'))
+            return true
+
+        return false
+    }
+
+    userLogout(){
+        this.authorize()
+        
+        return this.get('user/logout').then(response => {
+            if(response.data.success == true)
+            {
+                localStorage.removeItem("token")
+                localStorage.removeItem("username")
+                localStorage.removeItem("email")
+            }
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
+    getAllUserData(){
+        this.authorize()
+
+        return this.get('user/userdata').then(response => {
+            return response.data
+        }).catch(error => {
+            alert(error) // temponary
         })
     }
 }

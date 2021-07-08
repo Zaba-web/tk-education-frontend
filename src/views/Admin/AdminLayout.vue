228c43d12a8f866e569c0de7c5e2a8a5887dcfce
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-09 02:52:06 +0300

    Add color theme checking

--- src/views/Admin/AdminLayout.vue
+++ src/views/Admin/AdminLayout.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="dark-theme">
+    <div :class="{
+        'dark-theme': colorTheme == 'dark',
+        'light-theme': colorTheme == 'light'
+    }">
         <layout :menu-to-handle="menu">
             <transition 
                 enter-active-class="animate__animated animate__fadeInLeft"
@@ -11,21 +14,24 @@
 </template>
 
 <script>
-    import DashboardLayout from '../../components/Dashboard/DashboardLayout.vue'
-    import adminMenu from '../../static/adminMenu'
-    import "animate.css"
+    import DashboardLayout from '@/components/Dashboard/DashboardLayout.vue'
+    import adminMenu from '@/static/adminMenu'
 
     export default {
         data(){
             return {
-                menu: []
+                menu: [],
+                colorTheme: 'dark'
             }
         },
         components:{
             "layout": DashboardLayout
         },
         mounted(){
-           adminMenu.map(item => this.menu.push(item))
+
+            adminMenu.map(item => this.menu.push(item))
+            if(localStorage.getItem('colorTheme') != null) 
+                this.colorTheme = localStorage.getItem('colorTheme')
         }
     }
 </script>

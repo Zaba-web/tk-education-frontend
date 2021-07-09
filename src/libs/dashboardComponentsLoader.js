c140bffbeed9d39c3b54984bd22a9bced16fde76
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-10 00:22:29 +0300

    Add new components

--- src/libs/dashboardComponentsLoader.js
+++ src/libs/dashboardComponentsLoader.js
@@ -3,6 +3,10 @@ import DashboardSection from '@/components/Dashboard/DashboardSection.vue'
 import InlineContainer from '@/components/Dashboard/InlineContainer.vue'
 import DashboardDefaultBlock from '@/components/Dashboard/DashboardDefaultBlock.vue'
 import DashboardTable from '@/components/Dashboard/Table/DashboardTable.vue'
+import DashboardInstructionItem from '@/components/Dashboard/DashboardInstructionItem.vue'
+
+import ReloadEmiter from '@/components/Dashboard/ReloadEmiter.vue'
+import CreateEmiter from '@/components/Dashboard/CreateEmiter.vue'
 
 import OperationDelete from "@/components/Dashboard/Operations/OperationDelete.vue"
 import OperationPlain from "@/components/Dashboard/Operations/OperationPlain.vue"
@@ -13,7 +17,8 @@ const structureComponents = {
     "dashboard-section": DashboardSection,
     "inline-container": InlineContainer,
     "default-block": DashboardDefaultBlock,
-    "dashboard-table": DashboardTable
+    "dashboard-table": DashboardTable,
+    "instruction-item": DashboardInstructionItem,
 }
 
 const operationsComponents = {
@@ -22,4 +27,9 @@ const operationsComponents = {
     'edit': OperationEdit
 }
 
-export {structureComponents, operationsComponents}
+const listInterationComponents = {
+    'reloader': ReloadEmiter,
+    'creater': CreateEmiter
+}
+
+export {structureComponents, operationsComponents, listInterationComponents}

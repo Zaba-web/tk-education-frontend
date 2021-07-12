44716537a79e9acb19fdea2175b773167ae0f52e
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-12 19:05:34 +0300

    Add confirmed students list

--- src/views/Admin/Groups/AdminGroupInfo.vue
+++ src/views/Admin/Groups/AdminGroupInfo.vue
@@ -4,19 +4,41 @@
             Керування групами — {{groupData.name}}
         </dashboard-view-title>
         <dashboard-section title="Список учнів" subtitle="Зареєстровані та підтверджені учні">
-            
+            <dashboard-table :table-header="['Дата реєстрації', '№ у списку', 'Ім\'я', 'Підгрупа', 'Операції']">
+                <tr v-for="(student, studentIndex) of students.confirmed" :key="studentIndex" class="p-like sm-top-padding default-text-color less-size">
+                    <td >
+                        {{student.created_at}}
+                    </td>
+                    <td>
+                        {{student.student_number}}
+                    </td>
+                    <td>
+                        <div :class="`user-status user-status-${student.online}`">
+                            {{student.name}}
+                        </div>
+                    </td>
+                    <td>
+                        {{student.subgroup}}
+                    </td>
+                    <td style="width: 30px">
+                        <delete :path="`user/delete/${student.id}`" @deletedSuccessful="loadStudents"></delete>
+                    </td>
+                </tr>
+            </dashboard-table>
         </dashboard-section>
     </div>
 </template>
 
 <script>
     import {structureComponents} from "@/libs/dashboardComponentsLoader"
+    import {operationsComponents} from "@/libs/dashboardComponentsLoader"
 
     import API from '@/libs/api'
 
     export default {
         components: {
-            ...structureComponents
+            ...structureComponents,
+            ...operationsComponents
         },
         computed: {
             groupId(){
@@ -43,7 +65,12 @@
                     console.error(error)
                 })
             },
+
             loadStudents(){
+                this.students.all = [] 
+                this.students.confirmed = []
+                this.students.unconfirmed = []
+
                 this.api.getSecureData(`groups/${this.groupId}/students`).then(response => {
                     this.students.all = response
 
@@ -51,12 +78,14 @@
                         if(student.confirmed) this.students.confirmed.push(student)
                         else this.students.unconfirmed.push(student)
                     })
+
+                    this.students.confirmed.sort((a, b) => a.student_number > b.student_number)
                 })
             }
         },
+
         mounted(){
             this.api = new API()
-
             this.loadGroupData()
         }
     }

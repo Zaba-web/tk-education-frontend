36460e874f90788e931251f5b39ae695c3910ffb
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-13 22:12:52 +0300

    Change prop value type

--- src/views/Admin/Groups/AdminGroupsMain.vue
+++ src/views/Admin/Groups/AdminGroupsMain.vue
@@ -6,20 +6,20 @@
         <dashboard-section title="Загальна інформація" subtitle="Коротка довідка стосовно роботи системи">
             <default-block>
                <inline-container>
-                   <instruction-item item-title="Створення групи" filled="true">
+                   <instruction-item item-title="Створення групи" :filled="true">
                         Створити групу можна шляхом заповнення простої форми, де потрібно вказати назву групи та ім'я майстра в/н.
                    </instruction-item>
                     <instruction-item item-title="Реєстрація">
                         Після створення група стає доступною для вибору під час реєстрації учнів.
                    </instruction-item>
-                    <instruction-item item-title="Підтвердження" filled="true">
+                    <instruction-item item-title="Підтвердження" :filled="true">
                         З метою контролю коректності даних, підтвердження реєстрації учнів здійснюється вручну майстром в/н
                    </instruction-item>
                </inline-container>
             </default-block>
         </dashboard-section>
         <dashboard-section title="Зареєстровані групи" subtitle="Перелік зареєстрованих груп">
-            <group-list :reloadable='true' :can-add-new="true"></group-list>
+            <group-list :reloadable="true" :can-add-new="true"></group-list>
         </dashboard-section>
     </div>
 </template>

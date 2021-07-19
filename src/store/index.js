b9fa7ac36f6f8d124fbc4845e25f01de0b972453
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-19 21:49:49 +0300

    -

--- src/store/index.js
+++ src/store/index.js
@@ -47,15 +47,18 @@ export default createStore({
 			for(let item in state.inputSidebar.content)
 				state.inputSidebar.content[item] = false
 		},
+
 		SHOW_DASHBOARD_INPUT_SIDEBAR(state, payload){
 			state.inputSidebar.visible = true
 			state.inputSidebar.content[payload.component] = true
 			state.inputSidebar.data = payload.data
 		},
+
 		UPDATE_LIST(state, payload){
 			document.querySelector(payload).click()
 			state.lastUpdated = payload
 		},
+
 		ADD_NEW_MESSAGE(state, payload){
 			/* 
 				Payload should be an object with following stucture:
@@ -68,13 +71,9 @@ export default createStore({
 			state.messages.unshift(payload)
 			document.querySelector('.notification-trigger').classList.add('animate__animated', 'animate__shakeY')
 		},
+
 		REMOVE_MESSAGE(state, payload){
 			state.messages.splice(payload, 1)
 		}
-	},
-	actions: {
-
-	},
-	modules: {
 	}
 })
